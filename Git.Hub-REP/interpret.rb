a593d6513747d0ab07764551331f67ce60370a70
Khalid Azmatullah <khalidazy.2008@gmail.com>
2022-02-13 22:01:36 +0530

    Interpret.rb

--- Git.Hub-REP/interpret.rb
+++ Git.Hub-REP/interpret.rb
@@ -9,8 +9,23 @@ class It
     if loc_id == local_id
        File.open("localize.rb", "a+") do |file|
          file.write(" #localize
-localize = #{@loc} ")
+localize = '#{@loc}'
+writ = 'sven'")
        end
     end
   end
+  def gen(rand)
+    v = "id-info"
+    if rand == v
+      puts "ID-info"
+      rand_num = rand(12000..45677).to_i
+      puts rand_num
+      puts "Id-gen = '#{rand_num}'"
+    else
+      puts "rand-v"
+  end
 end
+interpreter = It.new(1700, "local", "Un-interupted")
+interpreter.localize(1700)
+interpreter.gen("id-info")
+interpreter.gen(1700)
